8f3e2af3d5c220118d8aca1a841cead84573fbf6
pcjellison <pcjellison@OBAMA>
2017-08-22 15:53:40 -0500

    Add project files.

---
+++ JellisonP_L1/JellisonP_L1.c
@@ -0,0 +1,14 @@
+#include <stdiio.h>
+int main()
+{
+	int base;
+	int power;
+
+	printf("Enter a base number and a power number");
+	scanf("%d %d", base, power);
+
+	if (base <= 0 || power <= 0)
+	{
+		printf
+	}
+}
